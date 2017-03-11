d46016c29ce790b142e62a34f12efe9e4f1cb705
Lukas Hechenberger <admin@ls-age.com>
2017-03-11 18:56:10 +0100

    feat(runStep): Add spinner

--- src/lib/utils/runStep.js
+++ src/lib/utils/runStep.js
@@ -1,9 +1,21 @@
 import colors from 'chalk';
+import { dots as spinner } from 'cli-spinner';
 
 export default function runStep(name, step) {
-  process.stdout.write(`- ${name}`);
+  let frame = -1;
+
+  function render() {
+    frame++;
+    if (frame === spinner.frames.length) {
+      frame = 0;
+    }
+    process.stdout.write(`${spinner.frames[frame]} ${name}`);
+  }
+
+  const interval = setInterval(render, spinner.interval);
 
   function end(sign) {
+    clearInterval(interval);
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     process.stdout.write(`${colors.green(sign)}${colors.bold(name)}\n`);
