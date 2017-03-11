eeddbbb086561a9954102079d3e0f66cfb201ccb
Lukas Hechenberger <admin@ls-age.com>
2017-03-11 18:41:36 +0100

    style(release): Cleanup runStep

--- src/lib/utils/runStep.js
+++ src/lib/utils/runStep.js
@@ -1,28 +1,21 @@
 import colors from 'chalk';
 
-let inStep = 0;
-
 export default function runStep(name, step) {
-  const inset = '  '.repeat(inStep);
-  inStep++;
+  process.stdout.write(`- ${name}`);
 
-  process.stdout.write(`${inset}-  ${name}`);
-
-  function clear() {
-    inStep--;
+  function end(sign) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
+    process.stdout.write(`${colors.green(sign)}${colors.bold(name)}\n`);
   }
 
   return step
     .then(res => {
-      clear();
-      process.stdout.write(`${inset}${colors.green('✔︎')}${colors.bold(name)}\n`);
+      end('✔︎');
       return res;
     })
     .catch(err => {
-      clear();
-      process.stdout.write(`${inset}${colors.red('✖')} ${colors.bold(name)}\n`);
+      end('✖');
       throw err;
     });
 }
