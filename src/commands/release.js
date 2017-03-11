c8f5d4a0a2b91cf1933442b625b3d74a52babc2a
Lukas Hechenberger <admin@ls-age.com>
2017-03-11 17:00:57 +0100

    feat(publish): Require confirmation for publishing

--- src/commands/release.js
+++ src/commands/release.js
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { prompt } from 'inquirer';
 import UsageError from '../lib/error/UsageError';
 import AppError from '../lib/error/AppError';
 import standardVersion from 'standard-version';
@@ -36,26 +37,14 @@ export const options = {
     type: 'boolean',
     default: true,
   },
+  'yes': {
+    desc: 'Publish without confirmation',
+    type: 'boolean',
+    default: false,
+  }
 };
 
-export function run(opts, env) {
-  if (opts.createRelease && !opts.githubToken) {
-    return Promise.reject(new UsageError('Missing option "github-token"'));
-  }
-
-  let releaseAs;
-  const releaseTypeOptions = [opts.major, opts.minor, opts.patch, opts.pre];
-  const chosenReleaseType = ['major', 'minor', 'patch', 'prerelease']
-    .filter((type, i) => releaseTypeOptions[i]);
-
-  if (chosenReleaseType.length > 1) {
-    return Promise.reject(new UsageError(
-      `Cannot release with multiple version types (${chosenReleaseType.join(', ')} specified)`
-    ));
-  } else if (chosenReleaseType.length === 1) {
-    releaseAs = chosenReleaseType[0];
-  }
-
+export function prepareRelease() {
   return runExternal('git status --short')
     .then(data => {
       if (data) {
@@ -64,12 +53,15 @@ export function run(opts, env) {
     })
     .then(() => runExternal('git push'))
     .then(() => runExternal('npm run prepublish'))
-    .then(() => runExternal('git add -f out'))
+}
+
+export function createRelease(opts, releaseAs) {
+  return runExternal('git add -f out')
     .then(() => runExternal('git add -f docs/api'))
     .then(() => new Promise((resolve, reject) => {
       standardVersion({
         infile: join(__dirname, '../../CHANGELOG.md'),
-        firstRelease: options.first,
+        firstRelease: opts.first,
         releaseAs,
         silent: true,
         commitAll: true,
@@ -84,5 +76,51 @@ export function run(opts, env) {
     .then(() => runExternal('git rm -rf out'))
     .then(() => runExternal('git rm -rf docs/api'))
     .then(() => runExternal(['git', 'commit', '-m', '"chore(release): Remove generated files"']))
-    .then(() => console.log('Now, create github release', opts.createRelease));
+}
+
+export function confirmPublish(opts) {
+  if (opts.yes) {
+    return Promise.resolve(true);
+  }
+
+  return prompt([
+    {
+      type: 'confirm',
+      name: 'release',
+      message: 'Publish release?',
+    }
+  ])
+    .then(answers => answers.release);
+}
+
+export function run(opts, env) {
+  // Validate required options
+  if (opts.createRelease && !opts.githubToken) {
+    return Promise.reject(new UsageError('Missing option "github-token"'));
+  }
+
+  // Evaluate releaseAs
+  let releaseAs;
+  const releaseTypeOptions = [opts.major, opts.minor, opts.patch, opts.pre];
+  const chosenReleaseType = ['major', 'minor', 'patch', 'prerelease']
+    .filter((type, i) => releaseTypeOptions[i]);
+
+  if (chosenReleaseType.length > 1) {
+    return Promise.reject(new UsageError(
+      `Cannot release with multiple version types (${chosenReleaseType.join(', ')} specified)`
+    ));
+  } else if (chosenReleaseType.length === 1) {
+    releaseAs = chosenReleaseType[0];
+  }
+
+  return this.prepareRelease(opts)
+    .then(() => createRelease(opts, releaseAs))
+    .then(() => confirmPublish(opts))
+    .then(publish => {
+      if (publish) {
+        console.log('Publish release');
+      } else {
+        console.log('Undo release');
+      }
+    })
 }
