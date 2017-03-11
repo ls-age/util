2099bc377ed9578e201a8ad3d05c362e4940a363
Lukas Hechenberger <admin@ls-age.com>
2017-03-11 18:33:44 +0100

    chore(release): 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="0.2.1"></a>
+## [0.2.1](https://github.com/ls-age/util/compare/v0.2.0...v0.2.1) (2017-03-11)
+
+
+### Features
+
+* **release:** Log sub steps ([ea7e051](https://github.com/ls-age/util/commit/ea7e051))
+
+
+
 <a name="0.2.0"></a>
 # [0.2.0](https://github.com/ls-age/util/compare/v0.1.2...v0.2.0) (2017-03-11)
 
---
+++ out/lib/utils/run.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = run;
+
+var _spawn = require('./spawn');
+
+var _spawn2 = _interopRequireDefault(_spawn);
+
+var _RunError = require('../error/RunError');
+
+var _RunError2 = _interopRequireDefault(_RunError);
+
+var _runStep = require('./runStep');
+
+var _runStep2 = _interopRequireDefault(_runStep);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function run(command, options) {
+  return (0, _runStep2.default)(command, (0, _spawn2.default)(command, options).then(child => {
+    return new Promise((resolve, reject) => {
+      let output = '';
+
+      child.stdout.on('data', d => output += d.toString());
+      child.stderr.on('data', d => output += d.toString());
+
+      child.on('error', err => reject(err));
+      child.on('close', code => {
+        if (code === 0) {
+          resolve(output);
+        } else {
+          reject(new _RunError2.default(command, code, { output }));
+        }
+      });
+    });
+  }));
+}
