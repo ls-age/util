262228f444082d172fcc29aec0cd492f7ade4aa1
Lukas Hechenberger <admin@ls-age.com>
2017-03-12 11:04:25 +0100

    "chore(release): Remove generated files"

--- out/bin/lsage.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _Util = require('../Util');
-
-var _Util2 = _interopRequireDefault(_Util);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_Util2.default.launch(process.argv.slice(2));
